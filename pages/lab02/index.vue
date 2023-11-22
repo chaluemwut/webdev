ea099c74247fe4e75c40f89a50f388f42b4904e3
Chaluemwut Noyunsan <chaluemwut@gmail.com>
2023-11-22 11:32:34 +0700

    add lab02

--- pages/lab02/index.vue
+++ pages/lab02/index.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
         <p>Hello lab 02</p>
+        <p>
+            <a href="/lab02/optional">optional</a>
+        </p>
+        <p>
+            <a href="/lab02/composition">composition</a>
+        </p>
     </div>
 </template>
---
+++ pages/lab02/optional.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  mounted() {
+    console.log(`The initial count is ${this.count}.`)
+  }
+}
+</script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
